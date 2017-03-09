2e592d6c9bd7c036d5b431d696d8b5acd1594efa
Jakob Krigovsky <jakob@krigovsky.com>
2017-03-09 19:03:25 +0100

    Experiment with Esprima

---
+++ experiment.js
@@ -0,0 +1,5 @@
+'use strict'
+const assert = require('./')
+const obj = {}
+
+assert(obj.foo).is('String')
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 'use strict';
+var fs = require('fs')
 var typeCheck = require('type-check').typeCheck;
+var getCallerPath = require('caller-path')
+var escallmatch = require('escallmatch')
+var esprima = require('esprima')
+var estraverse = require('estraverse')
+
+var matcher = escallmatch('assert(match)')
 
 module.exports = function (input) {
+	var callerPath = getCallerPath()
+
 	return {
 		is: function (type) {
 			var valid;
@@ -10,6 +19,17 @@ module.exports = function (input) {
 			if (typeof type === 'function') {
 				err = input + ' did not pass the test' + (type.name && ' of function `' + type.name + '`');
 				valid = type(input);
+			} else if (typeof input === 'undefined') {
+				estraverse.traverse(esprima.parse(fs.readFileSync(callerPath, 'utf8')), {
+					enter: (currentNode, parentNode) => {
+						if (matcher.test(currentNode)) {
+							var argument = currentNode.arguments[0]
+							var errInput = `${argument.object.name}.${argument.property.name}`
+							err = `Expected \`${errInput}\` to be of type \`${type}\`, got \`undefined\``
+							valid = false
+						}
+					}
+				})
 			} else {
 				err = input + ' is not of type ' + type;
 				valid = typeCheck(type, input);
